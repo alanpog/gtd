6f90eb42ec234d904c642b96647b5da9a45ea9d5
Jan Stolarek <jan.stolarek@p.lodz.pl>
2013-02-10 21:43:21 +0100

    Create config directory if it doesn't exist

--- src/GTD/Main.hs
+++ src/GTD/Main.hs
@@ -2,6 +2,37 @@ module Main (
   main
  ) where
 
+import System.Directory
+import System.Exit
+import System.FilePath
+import System.IO.Error
+
+appName :: String
+appName = "gtd"
+
+
+configFileName :: FilePath -> FilePath
+configFileName appDir = appDir ++ [pathSeparator] ++ ".gtdrc"
+
+
+configDirErrorMessage :: String
+configDirErrorMessage = "Error when locating GTD config directory"
+
+
+configDirCreateErrorMessage :: String
+configDirCreateErrorMessage = "Error when creating GTD config directory"
+
+
+handleIOErrors :: String -> IOError -> IO a
+handleIOErrors message e = do
+  print $ message ++ ": " ++ show e
+  exitFailure
+
+
 main :: IO ()
-main = putStrLn "GTD is not implemented yet"
- 
+main = do
+  appDirectory <- catchIOError (getAppUserDataDirectory appName)
+                               (handleIOErrors configDirErrorMessage)
+  catchIOError (createDirectoryIfMissing True appDirectory)
+               (handleIOErrors configDirCreateErrorMessage)
+  putStrLn "GTD is not implemented yet"
