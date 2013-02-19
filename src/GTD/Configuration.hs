eaead2e560971a62ba25554e06c60ab52c2889a8
Jan Stolarek <jan.stolarek@p.lodz.pl>
2013-02-19 19:26:56 +0100

    Convert config from file to internal representation

--- src/GTD/Configuration.hs
+++ src/GTD/Configuration.hs
@@ -5,26 +5,30 @@ module GTD.Configuration (
 
 import GTD.Assets
 
+import Control.Monad.Error
 import Data.ConfigFile
+import System.Exit
 
 data GTDConfig = GTDConfig {
       gtdCDateFormat      :: String
-    , gtdCShortDateFormat :: String
     , gtdCTasksDir        :: String
- }
+ } deriving Show
+
+
+gtdCP :: ConfigParser
+gtdCP = emptyCP {optionxform = id}
 
 
 defaultConfigString :: FilePath -> String
 defaultConfigString tasksDir =
   "dateFormat      = %d-%m-%Y\n"   ++
-  "shortDateFormat = %d-%m\n\n"    ++
   "tasksDir        = " ++ tasksDir ++
   "\n"
 
 
 defaultConfigParser :: FilePath -> ConfigParser
 defaultConfigParser tasksDir =
-    case readstring emptyCP (defaultConfigString tasksDir) of
+    case readstring gtdCP (defaultConfigString tasksDir) of
       Left _   -> error ("Error parsing built-in default configuration! " ++
                          "This should never happen. Please report this "  ++
                          "as a bug.")
@@ -39,7 +43,7 @@ initializeConfig configFile tasksDir =
 loadConfiguration :: FilePath -> FilePath -> IO GTDConfig
 loadConfiguration configFile tasksDir = do
     let defaultConfig = defaultConfigParser tasksDir
-    userConfig <- readfile emptyCP configFile
+    userConfig <- readfile gtdCP configFile
     config <- case userConfig of
                 Right cp -> return $ merge defaultConfig cp
                 Left err -> do
@@ -47,13 +51,18 @@ loadConfiguration configFile tasksDir = do
                           ++ configFile ++ ": " ++ show err ++ "\nDefault "
                           ++ "configuration will be used.\n")
                    return defaultConfig
-    validateConfiguration config
-    return (configParser2GTDConfig config)
+    configParser2GTDConfig config
 
 
-validateConfiguration :: ConfigParser -> IO ()
-validateConfiguration = undefined
-
-
-configParser2GTDConfig :: ConfigParser -> GTDConfig
-configParser2GTDConfig = undefined
+configParser2GTDConfig :: ConfigParser -> IO GTDConfig
+configParser2GTDConfig config = do
+  eitherEntries <- runErrorT $ items config "DEFAULT"
+  case eitherEntries of
+    Left _ -> do putStrLn ("Error while reading the DEFAULT section of "
+                     ++ "configuration file. This should never happen."
+                     ++ "Please report this as a bug.")
+                 exitFailure
+    Right entries -> return $ foldl setOption (GTDConfig "" "") entries
+    where setOption config (entry, value)
+              | entry == "dateFormat" = config {gtdCDateFormat = value}
+              | entry == "tasksDir"   = config {gtdCTasksDir   = value}
